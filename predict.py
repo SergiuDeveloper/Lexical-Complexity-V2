83a6ba13b8d563b867ae58d6f1e9ad062092dca2
SergiuDev <sergiu.nistor@info.uaic.ro>
2021-02-24 22:54:52 +0200

    Added snowball stemmer to reduce errors when detecting part of speech using WordNet

--- predict.py
+++ predict.py
@@ -2,19 +2,30 @@
 
 TRAIN_DATASET_PATH = './../CompLex/train/lcp_single_train.tsv'
 TEST_DATASET_PATH = './../CompLex/test-labels/lcp_single_test.tsv'
+LANGUAGE = 'english'
 
 from nltk.corpus import wordnet
+from nltk.stem import SnowballStemmer
 from nltk.wsd import lesk
 import pandas as pd
-import math
 
 
 def load_dataframe(dataframe_path):
     return pd.read_csv(dataframe_path, sep='\t')
 
 def get_tokens_POS(sentence_token_complexity_pairs):
+    stemmer = SnowballStemmer(LANGUAGE)
     tokens_POS = [(sentence, lesk(sentence, str(token)), complexity) for sentence, token, complexity in sentence_token_complexity_pairs]
-    #for sentence, token, complexity
+
+    for i in range(len(tokens_POS)):
+        sentence, token, complexity = tokens_POS[i]
+        if token is None:
+            word = sentence_token_complexity_pairs[i][1]
+            tokens_POS[i] = (sentence, lesk(sentence, stemmer.stem(word)), complexity)
+               
+    print([sentence_token_complexity_pairs[i][1] for i in range(len(tokens_POS)) if tokens_POS[i][1] is None])
+    print()
+
     return [(sentence, token, complexity) for sentence, token, complexity in tokens_POS if token is not None]
 
 
@@ -31,7 +42,7 @@ for _, test_token, test_complexity in test_tokens_POS:
         path_similarity = wordnet.path_similarity(test_token, train_token)
         if path_similarity is None:
             path_similarity = 0
-        path_similarities.append(math.fabs(path_similarity))
+        path_similarities.append(path_similarity)
         train_complexities.append(train_complexity)
     
     total_path_similarities = sum(path_similarities)
